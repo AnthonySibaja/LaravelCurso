69a8d1b3048c47e3339b5e4dc06233ad569634e4
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-08 13:39:33 -0600

    video 237
    se programan los métodos para poder almacenar los datos en la db y también su no hay nada escrito no insertara Campos vacíos

--- new_system/app/Http/Controllers/RoleController.php
+++ new_system/app/Http/Controllers/RoleController.php
@@ -1,16 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Role;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 class RoleController extends Controller
 {
     //
     public function index(){
-        return view('admin.role.index');
+
+        
+        return view('admin.role.index',[
+            'roles'=> Role::all()
+        ]);
     }
     public function store(){
-        dd(request('name'));
+        request()->validate([
+            'name'=>['required']
+        ]);
+        Role::create([
+            'name'=>Str::ucfirst(request('name')),
+            'slug'=>Str::of(Str::lower(request('name')))->slug('-'),
+
+        ]);
+        return back();
+        //dd(request('name'));
     }
 }
