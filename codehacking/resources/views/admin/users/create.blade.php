1f09d9b85764541e0821c2b5cd9ed52916197f82
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-12 14:07:36 -0600

    video 268
    creamos mensajes de error diciendo que los datos son obligatorios y después creamos una carpeta para poder almacenar las alertas en un mejor orden

--- codehacking/resources/views/admin/users/create.blade.php
+++ codehacking/resources/views/admin/users/create.blade.php
@@ -14,18 +14,21 @@
     </div>
     <div class="form-group">
         {!! Form::label('role_id', 'Role: ') !!}
-        {!! Form::select('role_id',[''=>'Choose Options']+ $roles , null, ['class' => 'form-control']) !!}
+        {!! Form::select('role_id', ['' => 'Choose Options'] + $roles, null, ['class' => 'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('status', 'Status: ') !!}
-        {!! Form::select('status',array(1=>'Active', 0=>'Not Active'), 0, ['class' => 'form-control']) !!}
+        {!! Form::select('status', [1 => 'Active', 0 => 'Not Active'], 0, ['class' => 'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('password', 'Password: ') !!}
-        {!! Form::password('password',array(1=>'Active', 0=>'Not Active'), 0, ['class' => 'form-control']) !!}
+        {!! Form::password('password', [1 => 'Active', 0 => 'Not Active'], 0, ['class' => 'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
     </div>
     {!! Form::close() !!}
+
+@include('includes.form_error')
+   
 @endsection
