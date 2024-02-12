208693d195e9b41406f1836d1de3e70df0f98f2c
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-12 12:07:28 -0600

    video 263
    se descargan dependencias para trabajar con laravel collective y se programa una form para create users

--- codehacking/config/app.php
+++ codehacking/config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -209,7 +210,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
-    ])->toArray(),
+        'Form'=>Collective\Html\formFacade::class,
+        'Html'=>Collective\Html\formFacade::class,
+        ])->toArray(),
 
 ];
