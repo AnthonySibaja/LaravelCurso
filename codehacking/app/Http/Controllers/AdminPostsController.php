72a2f0676bc7e49845540437339610487439458b
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-13 10:52:58 -0600

    video 289
    se crea una tabla donde se muestran los datos de post

--- codehacking/app/Http/Controllers/AdminPostsController.php
+++ codehacking/app/Http/Controllers/AdminPostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class AdminPostsController extends Controller
 {
@@ -14,7 +15,8 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
-        return view('admin.post.index');
+        $posts = Post::all();
+        return view('admin.post.index', compact('posts'));
     }
 
     /**
@@ -24,7 +26,7 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        //
+
         return view('admin.post.create');
     }
 
--- codehacking/resources/views/admin/post/index.blade.php
+++ codehacking/resources/views/admin/post/index.blade.php
@@ -2,4 +2,35 @@
 
 @section('content')
     <h1>Post</h1>
+
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">User id</th>
+                <th scope="col">Category id</th>
+                <th scope="col">Photo id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Body</th>
+                <th scope="col">Created</th>
+                <th scope="col">Updated</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if ($posts)
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>{{ $post->id }}</td>
+                        <td>{{ $post->user_id }}</td>
+                        <td>{{ $post->category_id }}</td>
+                        <td>{{ $post->photo_id }}</td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->body }}</td>
+                        <td>{{ $post->created_at ? $post->created_at->diffForHumans() : 'N/A' }}</td>
+                        <td>{{ $post->updated_at ? $post->updated_at->diffForHumans() : 'N/A' }}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
 @endsection
