73ede733d5645f9ac41bd2042b8bee10812bad37
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-13 11:56:41 -0600

    video 293
    solo se explican cosa y se comienza con el método store

--- codehacking/app/Http/Controllers/AdminPostsController.php
+++ codehacking/app/Http/Controllers/AdminPostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostsCreateRequest;
 
 class AdminPostsController extends Controller
 {
@@ -36,9 +37,10 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
-        return $request->all(); 
+        $user = Auth::user();
+        $user->posts;
      }
 
 
