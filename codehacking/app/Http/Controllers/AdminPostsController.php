e4c5ea133b70d5180cb9c274e9320359ab7053e2
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-13 16:00:48 -0600

    video 304
    se crea el metodo y se logra eliminar

--- codehacking/app/Http/Controllers/AdminPostsController.php
+++ codehacking/app/Http/Controllers/AdminPostsController.php
@@ -118,6 +118,13 @@ class AdminPostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::findOrFail($id);
+
+        unlink(public_path() . $post->photo->file);
+
+        $post->delete();
+        
+        return redirect('/admin/post');
+
     }
 }
