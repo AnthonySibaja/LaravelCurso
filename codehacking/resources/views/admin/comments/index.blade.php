352bec19e19dadbed196948b11f3f1409613f43c
Anthony Sibaja <sibajagranados@gmail.com>
2024-02-14 13:01:12 -0600

    323
    se crea el boton aprove y el delete

--- codehacking/resources/views/admin/comments/index.blade.php
+++ codehacking/resources/views/admin/comments/index.blade.php
@@ -3,43 +3,57 @@
 @section('content')
 
 @if ($comments->count() > 0)
-<h1>Comments</h1>
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Author</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Body</th>
-                        <th><a href="{{ route('home.post', ['id' => $comment->post->id]) }}">View Post</a></th>
-                        <th scope="col">Created</th>
-                        <th scope="col">Updated</th>
-                    </tr>
-                </thead>
-                <tbody>
+    <h1>Comments</h1>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Author</th>
+                <th scope="col">Email</th>
+                <th scope="col">Body</th>
+                <th>View Post</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comments as $comment)
+                <tr>
+                    <td>{{$comment->id}}</td>
+                    <td>{{$comment->author}}</td>
+                    <td>{{$comment->email}}</td>
+                    <td>{{$comment->body}}</td>
+                    <td><a href="{{ route('home.post', ['id' => $comment->post->id]) }}">View Post</a></td>
+                    <td>
+                        @if($comment->is_active == 1)
+                            {!! Form::open(['method'=>'PATCH', 'action'=> ['PostCommentsController@update', $comment->id]]) !!}
+                            <input type="hidden" name="is_active" value="0">
+                            <div class="form-group">
+                                {!! Form::submit('Un-approve', ['class'=>'btn btn-success']) !!}
+                            </div>
+                            {!! Form::close() !!}
+                        @else
+                            {!! Form::open(['method'=>'PATCH', 'action'=> ['PostCommentsController@update', $comment->id]]) !!}
+                            <input type="hidden" name="is_active" value="1">
+                            <div class="form-group">
+                                {!! Form::submit('Approve', ['class'=>'btn btn-info']) !!}
+                            </div>
+                            {!! Form::close() !!}
+                        @endif
+                        
+                        <td>
+                            {!! Form::open(['method'=>'DELETE', 'action'=> ['PostCommentsController@destroy', $comments->id]]) !!}
+                            <div class="form-group">
+                                {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+                            </div>
+                            {!! Form::close() !!}
+                        </td>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@else
+    <h1 class="text-center">No Comment</h1>
+@endif
 
-                    @foreach ($comments as $comments)
-                        <tr>
-                            
-
-                            <td>{{$comment->id}}</td>
-                            <td>{{$comment->author}}</td>
-                            <td>{{$comment->email}}</td>
-                            <td>{{$comment->body}}</td>
-
-                            <td>{{ $categories->created_at ? $categories->created_at->diffForHumans() : 'N/A' }}</td>
-
-                            <td>{{ $categories->updated_at ? $categories->updated_at->diffForHumans() : 'N/A' }}</td>
-                        </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
-            
-                
-            @else
-                <h1 class="text-center">No Comment</h1>
-            
-        @endif
-
+    
 @stop
